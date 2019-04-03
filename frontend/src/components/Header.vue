1242fd2e6addf32e23ab7e162ab0882db9a5bfbf
Niklas <niklaspor@gmail.com>
2019-04-03 16:15:12 +0200

    added sidebar & home

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  b-nav
+    b-nav-item Logo
+    b-nav-item Suche
+    b-nav-item Score
+    b-nav-item Einstellung
+</template>
---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  b-nav.w-25(vertical='')
+    b-nav-item(active='') Active
+    b-nav-item Link
+    b-nav-item Another Link
+    b-nav-item(disabled='') Disabled
+</template>
