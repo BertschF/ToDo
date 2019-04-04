c900e97656849b1d2941bb8800019916e7beb2f7
Niklas <niklaspor@gmail.com>
2019-04-04 16:16:46 +0200

    add icons to nav

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,8 +1,19 @@
 <template lang="pug">
   b-nav.wrapper(vertical='').bg-dark.w-25
-    b-nav-item(active='') Heute
-    b-nav-item Nächste 7 Tage
-    b-nav-item Alle
+    b-nav-item(active='') 
+      i.pr-2.fas.fa-calendar-day
+      | Heute
+    b-nav-item
+      i.pr-2.fas.fa-calendar-week
+      | Nächste 7 Tage
+    b-nav-item
+      i.pr-2.fas.fa-stream
+      | Alle
+    hr
+    span Projekte
+    hr
+    span Etiketten
+    hr
 </template>
 
 <style scoped lang="scss">
