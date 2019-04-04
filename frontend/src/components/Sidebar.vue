71ee9572735fa35ce261163031349acd487d8b30
Niklas <niklaspor@gmail.com>
2019-04-04 13:48:05 +0200

    dark color sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
-  b-nav.w-25(vertical='')
+  b-nav.wrapper(vertical='').bg-dark.w-25
     b-nav-item(active='') Active
     b-nav-item Link
     b-nav-item Another Link
     b-nav-item(disabled='') Disabled
 </template>
+
+<style scoped lang="scss">
+</style>
