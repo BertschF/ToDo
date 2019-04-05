ed40d03f74bc0b3cb016c8ec593ed213922f8e02
Niklas <niklaspor@gmail.com>
2019-04-05 09:28:54 +0200

    init store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import { ITag } from './model/tag';
+import { IProject } from './model/project';
+import { ITask } from './model/task';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    tasks: new Array<ITask>(),
+    projects: new Array<IProject>(),
+    tags: new Array<ITag>(),
   },
   mutations: {
 
