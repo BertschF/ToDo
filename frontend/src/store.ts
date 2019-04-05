d0212f612c9ad9c0fdef82b1709d4997faac7865
Niklas <niklaspor@gmail.com>
2019-04-05 11:08:08 +0200

    add dummy state & getters to store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -11,15 +11,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    tasks: new Array<ITask>(),
+    // tasks: new Array<ITask>(),
+    tasks: [
+      {
+        id: '129sug9asdfio',
+        description: 'Backend API implementieren',
+        state: State.Open,
+      },
+    ] as ITask[],
     projects: new Array<IProject>(),
     tags: new Array<ITag>(),
   },
   getters: {
-    openTasks: (state) => state.tasks.filter((task) => task.state === State.Open),
-    overDueTasks: (state) => state.tasks.filter((task) => before(task.dueDate, new Date(Date.now()))),
-    todayTasks: (state) => state.tasks.filter((task) => sameDay(task.dueDate, new Date(Date.now()))),
-    next7DaysTasks: (state) => state.tasks.filter((task) => before(task.dueDate, addDays(new Date(Date.now()), 7))),
+    openTasks: (state): ITask[] => state.tasks
+      .filter((task) => task.state === State.Open),
+    overDueTasks: (_, getters) => getters.openTasks
+      .filter((task: ITask) => before(task.dueDate, new Date(Date.now()))),
+    todayTasks: (_, getters) => getters.openTasks
+      .filter((task: ITask) => sameDay(task.dueDate, new Date(Date.now()))),
+    next7DaysTasks: (_, getters) => getters.openTasks
+      .filter((task: ITask) => before(task.dueDate, addDays(new Date(Date.now()), 7))),
   },
   mutations: {
 
