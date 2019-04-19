08f66faf47246119da33e1f9ffd65b05450851b2
Carsten Hagemann <carsten.hagemann@kaufland.com>
2019-04-19 17:57:27 +0200

    Show the amount of tasks of each category in the sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,9 +1,30 @@
 <template lang="pug">
   b-nav.wrapper(vertical='').w-25
     b-nav-item(active='', to='today') Heute
+      span.active-tasks {{ todayTasksAmount }}
     b-nav-item(to='upcoming') Nächste 7 Tage
-    b-nav-item(to='all') Alle
+      span.active-tasks {{ upcomingTasksAmount }}
+    b-nav-item(to='all') Alle offenen
+      span.active-tasks {{ openTasksAmount }}
 </template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default class Sidebar extends Vue {
+    get todayTasksAmount() {
+      return this.$store.getters.todayTasks.length;
+    }
+
+    get upcomingTasksAmount() {
+      return this.$store.getters.next7DaysTasks.length;
+    }
+
+    get openTasksAmount() {
+      return this.$store.getters.openTasks.length;
+    }
+  }
+</script>
 
 <style scoped lang="scss">
   .nav-link {
@@ -15,5 +36,9 @@
   .router-link-active {
     background-color: green;
     color: white;
+  }
+  
+  .active-tasks {
+    float: right;
   }
 </style>
